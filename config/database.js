0c158434373ab7a1f7739199e18cfc56138a1ecb
Manzil Junior <clonedbeast@Manzils-MacBook-Air.local>
2024-05-22 13:42:53 +0545

    minor fix

--- config/database.js
+++ config/database.js
@@ -1,17 +1,3 @@
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: "postgres",
-//     connection: {
-//       host: env("DATABASE_HOST", 'db-postgresql-nyc3-20591-do-user-14119558-0.c.db.ondigitalocean.com'),
-//       port: env("DATABASE_PORT", 25060),
-//       database: env("DATABASE_NAME", 'defaultdb'),
-//       user: env("DATABASE_USERNAME", 'doadmin'),
-//       password: env("DATABASE_PASSWORD", 'AVNS_1ECDAmPq9D2dSTz82wK'),
-//       schema: env("DATABASE_SCHEMA", 'public'),
-//     },
-//   },
-// });
-
 const path = require("path");
 
 module.exports = ({ env }) => ({
