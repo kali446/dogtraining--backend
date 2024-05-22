f00030879aabf456381bc2762d3701aa76567b9f
Manzil Junior <clonedbeast@Manzils-MacBook-Air.local>
2024-05-22 13:36:34 +0545

    changes done

--- config/plugins.js
+++ config/plugins.js
@@ -1,22 +1,17 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: "aws-s3",
+      provider: "strapi-provider-upload-do",
       providerOptions: {
-        accessKeyId: env("DO_SPACE_ACCESS_KEY"),
-        secretAccessKey: env("DO_SPACE_SECRET_KEY"),
+        key: env("DO_SPACE_ACCESS_KEY"),
+        secret: env("DO_SPACE_SECRET_KEY"),
         endpoint: env("DO_SPACE_ENDPOINT"),
-        params: {
-          Bucket: env("DO_SPACE_BUCKET"),
-        },
-      },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
+        space: env("DO_SPACE_BUCKET"),
+        directory: env("DO_SPACE_DIRECTORY"),
       },
     },
   },
+
 
   //   email: {
   //     config: {
